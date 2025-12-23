cd65ee6659665df8c7e61688b92dd05b97b6c42d
Grzegorz Miśków <133238725+gmaxsoft@users.noreply.github.com>
2025-12-23 18:22:03 +0100

    Dodanie pliku .env + utworznie domyślnej zmiennej ilości produktów na stronie

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 //import { useEffect, useMemo, useState } from 'react'
 
+const PRODUCTS_PER_PAGE = import.meta.env.VITE_PRODUCTS_PER_PAGE || 6
+
 function App() {
  
 
   return (
     <>
      
-      <h1>Shopping cart</h1>
+      <h1 className="text-3xl font-bold">Shopping cart</h1>
     
     </>
   )
